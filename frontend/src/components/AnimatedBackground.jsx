0160489c87d994a69a1b37ebea1fdb36ab4fad7c
itsmarcov <itsmarcov@users.noreply.github.com>
2026-06-25 19:43:49 +0200

    remove animated background particles

--- frontend/src/components/AnimatedBackground.jsx
+++
@@ -1,9 +0,0 @@
-export default function AnimatedBackground() {
-  return (
-    <ul className="bg-particles">
-      {Array.from({ length: 41 }, (_, i) => (
-        <li key={i} />
-      ))}
-    </ul>
-  );
-}
